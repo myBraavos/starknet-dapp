049dd839f1a044bbe5eb358528569aac26a0ca39
Abraham Makovetsky <avimak@gmail.com>
2022-06-10 12:03:34 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+import {
+    Box,
+    Button,
+    Container,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    ListSubheader,
+    Stack,
+    TextField,
+    Typography,
+} from "@mui/material";
+import { ContentCopy } from "@mui/icons-material";
+import { connect, getStarknet } from "get-starknet";
+import { Signature } from "starknet";
+import { ReactComponent as StarknetIcon } from "./starknet.svg";
+
+export const copyToClipboard = (value: string) => navigator.clipboard.writeText(value);
+
+const sign = async (
+    message: string,
+    network?: "goerli-alpha" | "mainnet-alpha"
+): Promise<Signature | undefined> => {
+    const wallet = getStarknet();
+    return !wallet.isConnected
+        ? undefined
+        : wallet.account.signMessage({
+              domain: {
+                  name: "StarkNet DApp",
+                  chainId:
+                      (network || "goerli-alpha") === "goerli-alpha"
+                          ? "SN_GOERLI"
+                          : "SN_MAIN",
+                  version: "0.0.1",
+              },
+              types: {
+                  StarkNetDomain: [
+                      { name: "name", type: "felt" },
+                      { name: "chainId", type: "felt" },
+                      { name: "version", type: "felt" },
+                  ],
+                  Message: [{ name: "message", type: "felt" }],
+              },
+              primaryType: "Message",
+              message: { message },
+          });
+};
+
+function Sign() {
+    const [message, setMessage] = useState<string>("");
+    const [signature, setSignature] = useState<string[]>();
+
+    return (
+        <Stack spacing={1} width={300}>
+            <Typography variant={"body1"}>{"Enter a message to sign"}</Typography>
+            <TextField
+                value={message}
+                onChange={e => setMessage(e.currentTarget.value ?? "")}
+            />
+            <Button
+                variant={"contained"}
+                disabled={!message?.trim()}
+                onClick={() => {
+                    sign(message)
+                        .then(signature => setSignature(signature))
+                        .catch(err => console.error(err));
+                }}>
+                {"Sign"}
+            </Button>
+            {!!signature?.length && (
+                <>
+                    <Box>
+                        <List dense disablePadding>
+                            <ListSubheader sx={{ mt: 2 }}>
+                                {"Signature"}
+                                <IconButton
+                                    size={"small"}
+                                    onClick={() => copyToClipboard(signature?.join(" "))}>
+                                    <ContentCopy fontSize={"small"} />
+                                </IconButton>
+                            </ListSubheader>
+
+                            {signature.map((val, idx) => (
+                                <ListItem key={val.substring(0, 5)}>
+                                    <ListItemText
+                                        primary={val}
+                                        secondary={idx === 0 ? "r" : "s"}
+                                    />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                </>
+            )}
+        </Stack>
+    );
+}
+
+function App() {
+    const [isConnected, setIsConnected] = useState<boolean>(getStarknet().isConnected);
+
+    useEffect(() => {
+        connect({ showList: false });
+    }, []);
+
+    return (
+        <Stack sx={{ height: "50vh" }} direction={"column"} justifyContent={"center"}>
+            <Container>
+                {isConnected ? (
+                    // sign message
+                    <Sign />
+                ) : (
+                    // connect
+                    <>
+                        <Stack direction={"row"} alignItems={"center"}>
+                            <StarknetIcon width={40} />
+                            <Typography variant={"h5"}>{"Welcome!"}</Typography>
+                        </Stack>
+
+                        <Box>
+                            <Typography variant={"body1"} sx={{ mt: 2 }}>
+                                {"Please connect with your wallet"}
+                            </Typography>
+                            <Button
+                                sx={{ mt: 1 }}
+                                variant={"contained"}
+                                onClick={async () => {
+                                    try {
+                                        const wallet = await connect({
+                                            include: ["braavos"],
+                                        });
+                                        if (wallet) {
+                                            await wallet.enable({ showModal: true });
+                                            setIsConnected(!!wallet?.isConnected);
+                                        }
+                                    } catch {}
+                                }}>
+                                {"Connect Wallet"}
+                            </Button>
+                        </Box>
+                    </>
+                )}
+            </Container>
+        </Stack>
+    );
+}
+
+export default App;
